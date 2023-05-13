c1f6a45f68da4350e55b060024e17fba3b3bfd48
Prizzz <rob_243248@mail.ru>
2023-05-13 17:25:57 +0400

    fix: fix pages for sm screens

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './pages/main/Main';
 import Compatibility from './pages/compatibility/Compatibility';
 import Horoscope from './pages/horoscope/Horoscope';
